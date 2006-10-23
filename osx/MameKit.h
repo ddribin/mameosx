e80a33f97fa5d43dcde6d3aaf9633f129843f5e2
Dave Dribin <dave@dribin.org>
2006-10-23 03:34:30 +0000

    Create MameKit.framework and full MAME OS X.app with embedded framework

---
+++ osx/MameKit.h
@@ -0,0 +1,15 @@
+/*
+ *  MameKit.h
+ *  mameosx
+ *
+ *  Created by Dave Dribin on 10/22/06.
+ *  Copyright 2006 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import "MameAudioController.h"
+#import "MameConfiguration.h"
+#import "MameFileManager.h"
+#import "MameRenderer.h"
+#import "MameTimingController.h"
+#import "MameView.h"
