8603bcabeeff5a1d1a1e8cc32d71c7f175c23f2c
Dave Dribin <dave@dribin.org>
2006-11-16 18:34:52 +0000

    Only clone arrays and hashes

--- tools/hashes2ostruct.rb
+++ tools/hashes2ostruct.rb
@@ -1,14 +1,15 @@
 require 'ostruct'
 
 def hashes2ostruct(object)
-  object = object.dup
   return case object
   when Hash
+    object = object.clone
     object.each do |key, value|
       object[key] = hashes2ostruct(value)
     end
     OpenStruct.new(object)
   when Array
+    object = object.clone
     object.map! { |i| hashes2ostruct(i) }
   else
     object
