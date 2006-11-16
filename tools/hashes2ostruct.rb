30eff530d91594f4f061eeec1c8325d573df8ce4
Dave Dribin <dave@dribin.org>
2006-11-16 18:56:15 +0000

    Add unit tests

--- tools/hashes2ostruct.rb
+++ tools/hashes2ostruct.rb
@@ -15,3 +15,37 @@ def hashes2ostruct(object)
     object
   end
 end
+
+if __FILE__ == $0
+  require 'test/unit'
+  
+  class Tashes2ostructTest < Test::Unit::TestCase
+    def test_hash
+      struct = hashes2ostruct({"foo" => "bar", "colors" => ["red", "blue"]})
+      assert_equal(struct.foo, "bar")
+      assert_equal(struct.colors, ["red", "blue"])
+      assert_nil(struct.bar)
+    end
+    
+    def test_integer
+      struct = hashes2ostruct({"foo" => 5})
+      assert_equal(struct.foo, 5)
+    end
+    
+    def test_array
+      struct = hashes2ostruct([{"foo" => "bar"}, {"baz" => "bing"}])
+      assert_equal(struct[0].foo, "bar")
+      assert_equal(struct[1].baz, "bing")
+    end
+    
+    def test_nested
+      hash = {
+        "foo" => "bar",
+        "baz" => {"colors" => ["red", "blue"]}
+      }
+      struct = hashes2ostruct(hash)
+      assert_equal(struct.foo, "bar")
+      assert_equal(struct.baz.colors[1], "blue")
+    end
+  end
+end
