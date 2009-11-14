6282a52bcf0a66452655cd2f5276221346b25b81
Dave Dribin <dave@dribin.org>
2009-11-13 22:10:36 -0600

    Change DBGOBJS to the new DASMOBJS

--- tools/source_dump.mk
+++ tools/source_dump.mk
@@ -7,7 +7,7 @@ include mame/makefile
 # Using a := forces all variables and functions to be evaluated
 OSX_DRVLIBS := $(DRVLIBS)
 OSX_CPUOBJS := $(CPUOBJS)
-OSX_DBGOBJS := $(DBGOBJS)
+OSX_DBGOBJS := $(DASMOBJS)
 OSX_SOUNDOBJS := $(SOUNDOBJS)
 OSX_CPUDEFS := $(CPUDEFS)
 OSX_SOUNDDEFS := $(SOUNDDEFS)
